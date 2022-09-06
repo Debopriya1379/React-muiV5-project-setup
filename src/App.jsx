639db528621c438f998e9b0c220a7161d7bc1829
Debopriya Das <debopriyadas721@gmail.com>
2022-09-06 13:21:19 +0530

    project set up with mui-v5

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import {Box,Typography} from '@mui/material'
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <Box>
+      <Navbar/>
+      <Typography variant='h6'>Start writing your react app with mui</Typography>
+    </Box>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import{AppBar,Toolbar} from '@mui/material'
+
+export default function Navbar() {
+  return (
+    <AppBar position='sticky'>
+        <Toolbar>
+            hi this is your navbar
+        </Toolbar>
+    </AppBar>
+  )
+}
